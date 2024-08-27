81ec3913b945c17b69ed2f2831234a06548392ef
humbertohonorato <humberto.honorato@outlook.com>
2024-08-27 17:13:02 -0300

    Revert "teste comentando tudo kk"
    This reverts commit 1242b66ed76fe7ff13bf53a94d4bb21baf835c07.

--- src/main/java/com/cf/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/cf/authuser/controllers/AuthenticationController.java
@@ -1,34 +1,34 @@
-//package com.cf.authuser.controllers;
-//
-//import com.cf.authuser.dtos.UserDto;
-//import com.cf.authuser.enums.UserType;
-//import com.cf.authuser.models.UserModel;
-//import com.cf.authuser.services.impl.UserServiceImpl;
-//import org.springframework.beans.BeanUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.time.LocalDateTime;
-//import java.time.ZoneId;
-//
-//@RestController
-//@CrossOrigin(origins = "*", maxAge = 3600)
-//@RequestMapping("/auth")
-//public class AuthenticationController {
-//
-//    @Autowired
-//    UserServiceImpl userService;
-//
-//    @PostMapping("/signup")
-//    public ResponseEntity<Object> registerUser (@RequestBody UserDto userDto){
-//        UserModel userModel = new UserModel();
-//        BeanUtils.copyProperties(userDto, userModel);
-//        userModel.setUserType(UserType.USER);
-//        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
-//        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
-//        userService.save(userModel);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
-//    }
-//}
+package com.cf.authuser.controllers;
+
+import com.cf.authuser.dtos.UserDto;
+import com.cf.authuser.enums.UserType;
+import com.cf.authuser.models.UserModel;
+import com.cf.authuser.services.impl.UserServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser (@RequestBody UserDto userDto){
+        UserModel userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        userModel.setUserType(UserType.USER);
+        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userService.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+}
