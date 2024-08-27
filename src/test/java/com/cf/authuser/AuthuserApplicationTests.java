902fc1cbec435ed7b7639d894d4e6cc373e2c18d
humbertohonorato <humberto.honorato@outlook.com>
2024-08-27 20:44:49 -0300

    configurando testes

--- src/test/java/com/cf/authuser/AuthuserApplicationTests.java
+++ src/test/java/com/cf/authuser/AuthuserApplicationTests.java
@@ -1,15 +1,18 @@
-//package com.cf.authuser;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//@SpringBootTest
-//@ActiveProfiles("test")
-//class AuthuserApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.cf.authuser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+class AuthuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
