f0b3625305755842c742932fa58c3d9bdacfed05
humbertohonorato <humberto.honorato@outlook.com>
2024-08-27 20:33:33 -0300

    comentando testes

--- src/test/java/com/cf/authuser/AuthuserApplicationTests.java
+++ src/test/java/com/cf/authuser/AuthuserApplicationTests.java
@@ -1,15 +1,15 @@
-package com.cf.authuser;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
-class AuthuserApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.cf.authuser;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class AuthuserApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
