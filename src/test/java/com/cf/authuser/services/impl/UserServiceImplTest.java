f9064347f5fd72dcd88cf34365d4cd37a7c33b95
humbertohonorato <humberto.honorato@outlook.com>
2024-09-27 21:40:40 -0300

    configurando testes e implemenrando no ci cd verificacao de testee e cobertura

--- src/test/java/com/cf/authuser/services/impl/UserServiceImplTest.java
+++ src/test/java/com/cf/authuser/services/impl/UserServiceImplTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -17,22 +20,38 @@ import static org.mockito.Mockito.*;
 
 public class UserServiceImplTest {
 
-    @InjectMocks
-    private UserServiceImpl userService;
-
     @Mock
     private UserRepository userRepository;
+
+    @Autowired
+    @InjectMocks
+    private UserServiceImpl userService;
 
     private UserModel user;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.initMocks(this);
 
         user = new UserModel();
         user.setUserId(UUID.randomUUID());
         user.setEmail("test@example.com");
         user.setUserName("Test User");
+    }
+
+    @Test
+    void testFindAll_UserExists() {
+        List<UserModel> userList = new ArrayList<>();
+        userList.add(user);
+
+        when(userRepository.findAll()).thenReturn(userList);
+
+        List<UserModel> result = userService.findAll();
+
+        assertEquals(1, result.size());
+        assertEquals(user.getEmail(), result.get(0).getEmail());
+
+        verify(userRepository, times(1)).findAll();
     }
 
     @Test
@@ -58,10 +77,34 @@ public class UserServiceImplTest {
 
     @Test
     void testDelete_UserExists() {
-        when(userRepository.findById(any(UUID.class))).thenReturn(Optional.of(user));
+        when(userRepository.findById(any())).thenReturn(Optional.of(user));
 
         userService.delete(user.getUserId());
 
         verify(userRepository, times(1)).delete(user);
+    }
+
+    @Test
+    void testDelete_UserNotFound() {
+        when(userRepository.findById(any())).thenReturn(Optional.empty());
+
+        RuntimeException  exception = assertThrows(RuntimeException.class, () -> {
+            userService.delete(UUID.randomUUID());
+        });
+        assertEquals("User not found", exception.getMessage());
+    }
+
+    @Test
+    void testSave_User(){
+        when(userRepository.save(any())).thenReturn(user);
+        userService.save((user));
+        verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    void testUpdate_User(){
+        when(userRepository.findById(any(UUID.class))).thenReturn(Optional.of(user));
+        userService.updateUser(user.getUserId(), user);
+        verify(userRepository, times(1)).save(user);
     }
 }
